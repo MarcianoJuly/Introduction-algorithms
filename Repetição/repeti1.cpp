2c0e4fc4a5e54edb84b344fb92179ed71856008d
Julio Mariano <111182666+MarcianoJuly@users.noreply.github.com>
2022-11-08 17:15:02 -0300

    Create repeti1.cpp

---
+++ Repetição/repeti1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int divi,den,quan=0,resul=0;
+	
+	cin>>divi;
+	while(resul<divi){
+		cin>>den;
+		quan++;
+		if(divi>den){
+			resul += den;
+		}			
+	}
+	cout<<quan<<endl;
+	return 0;
+}
+		
